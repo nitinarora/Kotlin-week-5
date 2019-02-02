8b4c9d3d7c7c0e60a78726b173a976852ce1b810
nitin <nitinarora1981@gmail.com>
2019-02-02 17:14:14 +0100

    Partial implementation of game of 15 instance

--- Games/Task/src/games/gameOfFifteen/GameOfFifteen.kt
+++ Games/Task/src/games/gameOfFifteen/GameOfFifteen.kt
@@ -4,11 +4,49 @@ import board.Direction
 import board.GameBoard
 import board.createGameBoard
 import games.game.Game
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 /*
  * Implement the Game of Fifteen (https://en.wikipedia.org/wiki/15_puzzle).
  * When you finish, you can play the game by executing 'PlayGameOfFifteen'.
  */
 fun newGameOfFifteen(initializer: GameOfFifteenInitializer = RandomGameInitializer()): Game =
-    TODO()
+        object : Game {
+            private var gb: GameBoard<Int?> = createGameBoard(4)
+            lateinit var currentPermutation: List<Int>
+
+            override fun initialize() {
+                currentPermutation = initializer.initialPermutation
+                val randomIndex = Random.nextInt(0..15)
+                gb.getAllCells().forEachIndexed{ id, cell ->
+                    when (id) {
+                        randomIndex -> gb[cell] = null
+                        else -> gb[cell] = currentPermutation[id]
+                    }
+                }
+            }
+
+            override fun canMove(): Boolean = isEven(currentPermutation)
+
+            override fun hasWon(): Boolean {
+                val allCells = gb.getAllCells()
+
+                allCells.forEachIndexed { index, cell ->
+                    if (index <15 && ( gb[cell] == null ||  gb[cell] != index)) {
+                        return false
+                    }
+                }
+                return true
+            }
+
+            override fun processMove(direction: Direction) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun get(i: Int, j: Int): Int? {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        }
 
